62a8da542e2e709a074f4eb337be3348c7f40910
root <aditya.verma1044@gmail.com>
2020-10-28 20:04:14 +0530

    build

--- bot.py
+++ bot.py
@@ -5,13 +5,19 @@ import os
 import time
 import threading
 import subprocess
+from apscheduler.schedulers.background import BackgroundScheduler
 
+intents = discord.Intents.default()
+intents.members = True 
+
+sched = BackgroundScheduler()
+sched.start()
 load_dotenv()
 TOKEN=os.getenv('DISCORD_TOKEN')
 GUILD=os.getenv('GUILD')
 
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -19,11 +25,12 @@ async def on_ready():
         f'Logged in as {client.user}\n'        
     )
     print(client.guilds)
+    await client.wait_until_ready()
     user=client.get_user(int(os.getenv('ME')))
-    await user.send('Hello')
-    threading.Timer(10,execute, ()).start()  
-
-
+    #await user.send('Hello')
+    in_sec=int(os.getenv('DELAY'))                        #Change this
+    print(in_sec)
+    sched.add_job(test,'interval', seconds = in_sec)
 
 @client.event
 async def on_message(message):
@@ -47,37 +54,54 @@ async def on_message(message):
                 command_file.write(com)
                 await message.author.send("Added!")
                 command_file.close()
-
-async def custom():
-    process = subprocess.Popen("echo nice", shell=True, stdout=subprocess.PIPE)
-    process.wait()
-    print(process.returncode)
-    user=client.get_user(int(os.getenv('ME')))
-    await user.send('Halo')
-    print("OK")
+def test():
+    print("Worked")
 
 def execute():
-    pwd=subprocess.check_output("pwd",shell=True)
-    commands=open(pwd.decode('utf-8').strip()+"/tmp/commands_list)","r+")    #Why it is not able to read the file
-    domains=open(pwd.decode('utf-8').strip()+"/tmp/domains_list","r+")
+    commands=open("./tmp/commands_list","r+")   
+    domains=open("./tmp/domains_list","r+")
     lines=commands.readlines()
+    counter=0
     for line in lines:
+        counter+=1
         command=line.strip()
+        threads=[]
         lines2=domains.readlines()
         for l in lines2:
             domain=l.strip()
-            t=threading.Thread(target=run, args=(command,domain,))
+            t=threading.Thread(target=run, args=(command,domain,counter,))
             threads.append(t)
             t.start()
         for x in threads:
             x.join()
+        domains.seek(0)
+    commands.close()
+    domains.close() 
 
-def run(what,where):
-    cmd=what+where+" >> ./tmp/"+where+"_sub.txt"
+def run(what,where,flag):
+    cmd=what+" "+where+" > ./tmp/"+where+"_sub.txt" 
+    cmd2=what+" "+where+" >> ./tmp/"+where+"_sub.txt"                              
     print(cmd)
-    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    process.wait()       
+    if(flag == 1):
+        process = subprocess.Popen(cmd, shell=True)
+        process.wait()
+    else:
+        process = subprocess.Popen(cmd2, shell=True)
+        process.wait()
+       
 
+def check():
+    sites=open("./tmp/domains_list","r+")
+    sites_lines=sites.readlines()
+    loc="./tmp/"
+    for a in sites_lines:
+        fname=a.strip()+"_sub.txt"
+        f2name="."+fname
+        if(os.path.isfile(loc+f2name):
+            #adasdasd
+        else:
+            subprocess.Popen("cp "+loc+fname+" "+loc+f2name,shell=True)
+        
 #asyncio.ensure_future(custom(client))
-
+#diff file1 file2 | grep "<" | sed 's/^<//g'  > file3
 client.run(TOKEN)
