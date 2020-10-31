ef0bf393740396566fee6186cf9fe86c01eb14e6
root <aditya.verma1044@gmail.com>
2020-10-31 20:06:36 +0530

    build

--- bot.py
+++ bot.py
@@ -3,9 +3,11 @@ import subprocess
 from dotenv import load_dotenv
 import os
 import time
+import asyncio
 import threading
 import subprocess
 from apscheduler.schedulers.background import BackgroundScheduler
+from discord.ext import tasks
 
 intents = discord.Intents.default()
 intents.members = True 
@@ -25,37 +27,46 @@ async def on_ready():
         f'Logged in as {client.user}\n'        
     )
     print(client.guilds)
-    await client.wait_until_ready()
     user=client.get_user(int(os.getenv('ME')))
     #await user.send('Hello')
     in_sec=int(os.getenv('DELAY'))                        #Change this
     print(in_sec)
-    sched.add_job(test,'interval', seconds = in_sec)
-
+    await test.start()
+    #chec=sched.add_job(test,'interval', seconds = 10)        #Figure out how to call the functions => call execute and check
+ 
 @client.event
 async def on_message(message):
     if(message.author == client.user):      #Ignore messages by bot itself
 	    return
 
     prefix="track"
-    words=message.content.split(' ')
-    if(words[0].lower() == prefix ):
-        if(words[1].lower() == "add"):
-            if(words[2].lower() == "domain"):
-                print("OK")
-                dom=words[3]
-                domain_file=open("./tmp/domains_list","a+")
-                domain_file.write(dom)			     
-                await message.author.send("Added!")
-                domain_file.close()
-            elif(words[2].lower() == "command"):
-                com=words[3]
-                command_file=open("./tmp/commands_list","a+")
-                command_file.write(com)
-                await message.author.send("Added!")
-                command_file.close()
-def test():
+    if(message.author==client.get_user(int(os.getenv('ME')))):
+        words=message.content.split(' ')
+        if(words[0].lower() == prefix ):
+            if(words[1].lower() == "add"):
+                if(words[2].lower() == "domain"):
+                    print("OK")
+                    dom=words[3]
+                    domain_file=open("./tmp/domains_list","a+")
+                    domain_file.write('\n')
+                    domain_file.write(dom)			     
+                    await message.author.send("Added!")
+                    domain_file.close()
+                elif(words[2].lower() == "command"):
+                    com=words[3]
+                    command_file=open("./tmp/commands_list","a+")
+                    command_file.write('\n')
+                    command_file.write(com)
+                    await message.author.send("Added!")
+                    command_file.close()
+
+@tasks.loop(seconds=10.0)
+async def test():                     #Problem: Holds the main thread
     print("Worked")
+    await atest()
+    time.sleep(10)
+    #test2()              =>     Run this function in background
+    print("Awake")
 
 def execute():
     commands=open("./tmp/commands_list","r+")   
@@ -97,11 +108,23 @@ def check():
     for a in sites_lines:
         fname=a.strip()+"_sub.txt"
         f2name="."+fname
-        if(os.path.isfile(loc+f2name):
-            #adasdasd
+        if(os.path.isfile(loc+f2name)):
+            subprocess.Popen("diff "+loc+fname+" "+loc+f2name+" | grep '<' | sed 's/^< //g' > ./tmp/changes_"+a.strip(),shell=True).wait()          
+            subprocess.Popen("cp "+loc+fname+" "+loc+f2name,shell=True).wait()
         else:
-            subprocess.Popen("cp "+loc+fname+" "+loc+f2name,shell=True)
-        
-#asyncio.ensure_future(custom(client))
-#diff file1 file2 | grep "<" | sed 's/^<//g'  > file3
+            subprocess.Popen("cp "+loc+fname+" "+loc+f2name,shell=True).wait()
+        #if(os.path.isfile(loc+"changes_"+a.strip()):
+            
+    sites.close()
+
+async def atest():
+    user1=client.get_user(int(os.getenv('ME')))
+    print("SSleeping")
+    #time.sleep(10)
+    await user1.send("OK")
+
+def test2():
+    time.sleep(20)
+    print("Slept")
+    
 client.run(TOKEN)
